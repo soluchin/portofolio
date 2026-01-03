8b7cc5a8f1eab5defed606c73e15ea3c0c5431ff
soluchin <rafisolichin@gmail.com>
2026-01-03 16:33:07 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ProfilePic from './assets/profile.jpg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hi, I'm Rafi Solichin</h1>
+        <img src={ProfilePic} alt="Rafi Solichin" width={200} />
+      </div>
+    </>
+  )
+}
+
+export default App
